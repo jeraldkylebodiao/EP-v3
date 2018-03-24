432b330e7acf7a374850d0e01ac983d8356c989c
Joe Cris_ <johnpalafox025@gmail.com>
2018-03-24 21:38:11 +0800

    Add files via upload

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,2 +1,50 @@
- </body>  
- </html>  
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+            
+        <h4 style="color:black; font-size:20px;"></h4>
+        <ul class="list-inline mb-0">
+              <li class="list-inline-item mr-3">
+
+               
+        </div>
+          <div class="col-md-4">
+           
+        <p>&copy;Copyright 2018 - EXPLOREPINAS. All rights reserved.</p>
+          </div>
+          <div class="col-md-4">
+            <h4 style="color:black;">Contact Us</h4>
+        <ul class="list-inline mb-0">
+              <li class="list-inline-item mr-3">
+
+                <a href="#" style="color:black;">
+                  <i class="fa fa-facebook fa-2x fa-fw"></i>
+                </a>
+              </li>
+              <li class="list-inline-item mr-3">
+                <a href="#" style="color:black;">
+                  <i class="fa fa-twitter fa-2x fa-fw"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#" style="color:black;">
+                  <i class="fa fa-instagram fa-2x fa-fw"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+      </div>  
+    </div>
+  </footer>
+
+    <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.easing.min.js"></script> 
+    <script src="<?php echo base_url();?>assets/js/jquery.scrollTo.js"></script>
+    <script src="<?php echo base_url();?>assets/js/wow.min.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/custom.js"></script>
+
+</body>
+</html>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,28 +1,115 @@
-<!DOCTYPE html>  
- <html>  
- <head>  
-      <title>EXPLORE PINAS</title>  
-   
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-		<style >
-			body{
-				font-family: century gothic;
-				text-align: center;
-			}
-		
-			label{
-				
-			
-				font-size: 20px;
-			}
-			#msg{
-				font-size: 15px;
-			}
+<!DOCTYPE html>
+<html lang="en">
 
-		</style>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    
+  <title>EXPLORE PINAS</title>  
+      
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Fonts -->
+    <link href="<?php echo base_url(); ?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url(); ?>assets/css/animate.css" rel="stylesheet" />
+    <!-- Squad theme CSS -->
+    <link href="<?php echo base_url(); ?>assets/css/tour.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/color/default.css" rel="stylesheet">
+  <!--include bootstrap social -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-social.css">
+<style>
+      #posts{
+          background-color: white;
+          margin-bottom: 20px;
+          padding: 10px;
+          border-radius: 2px;
+          width: 90%;        
+        }
+        
+        </style>
 </head>
- <body>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+    <!-- Preloader -->
+    <div id="preloader">
+      <div id="load"></div>
+    </div>
+
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+					<ul class="nav navbar-nav">
+					<li><a href="<?php echo base_url();?>"><font font size="5">EXPLOREPINAS</font></a></li>
+				</ul>
+				</div>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="<?php echo base_url().'dashboard/user'; ?>">Home</a></li>
+        
+     
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Places <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            
+            <li><a href="<?php echo base_url().'dashboard/tourist_spot'; ?>">Tourist Spot</a></li>
+
+            <li><a href="<?php echo base_url().'dashboard/editorspick'; ?>">Editor's Choice</a></li>
+
+          </ul>
+        </li>
+  
+      <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="<?php echo base_url().'dashboard/viewprofile'; ?>">View Profile</a></li>
+  			
+     
+            <li><a href="<?php echo base_url().'aishiteru/logout'; ?>">Log Out</a></li>          
+          </ul>
+        </li>
+      </ul>
+  </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <br><br><br><br><br>
+     
+      
+  <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.easing.min.js"></script> 
+    <script src="<?php echo base_url();?>assets/js/jquery.scrollTo.js"></script>
+    <script src="<?php echo base_url();?>assets/js/wow.min.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/custom.js"></script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+      
+
+
+
+    
